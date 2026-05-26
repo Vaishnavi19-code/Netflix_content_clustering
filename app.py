b465c01803aecae728bdc48e15985fa1bef29da8
Vaishnavi19-code <vaishnavishakki19@gmail.com>
2026-05-26 19:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 def explain(text):
     response = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.1-8b-instant",
         messages=[
             {"role": "user", "content": f"Explain this Netflix content: {text}"}
         ]
